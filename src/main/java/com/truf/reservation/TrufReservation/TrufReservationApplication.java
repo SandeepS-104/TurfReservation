3bc47f83c05268fa17df703b62fec64aca8e26e7
Sandeep <saisanjay647@gmail.com>
2024-05-02 11:06:35 +0530

    initial setup of dependencies

---
+++ src/main/java/com/truf/reservation/TrufReservation/TrufReservationApplication.java
@@ -0,0 +1,13 @@
+package com.truf.reservation.TrufReservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrufReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrufReservationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/truf/reservation/TrufReservation/TrufReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.truf.reservation.TrufReservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrufReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
